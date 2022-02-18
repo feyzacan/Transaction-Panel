2de4089533ecf4a5716e6e72ba3606eb61be72ba
Feyza Yiğiter <feyzayigiter@gmail.com>
2022-02-18 20:19:53 +0300

    cancel button

--- app.js
+++ app.js
@@ -1,149 +1,201 @@
 const state = {
-    userList: []
+    userList: [],
+    transaction: []
 }
-
+// UPDATE USERLIST
 function Li(list, subscriber) {
+    subscriber.innerHTML = ""
     list.forEach(function (item) {
+        // CREATE NEW LIST ELEMENT
         const newLi = document.createElement("li")
+        // CREATE A LIST OF USERNAMES AND BALANCES
         newLi.innerHTML = `<div class="ms-2 me-auto">${item.name}</div><div class="me-4">${item.balance}</div>
         <button class="btn btn-danger btn-sm" onclick="removeUser(${item.id})">Del</button>`
-        newLi.setAttribute("id", item.id)
         newLi.setAttribute("class", "list-group-item d-flex justify-content-between align-items-center")
+        // RENDER THE LIST ON THE USER AREA
         subscriber.appendChild(newLi)
     })
 }
 
-function Hi(list, subscriber) {
+// UPDATE HISTORYLIST
+function Hi(list, subscriber, addition) {
+    //DELETION
     if (subscriber === "del") {
+        // CREATE NEW ELEMENT FOR DELETION
         const newDel = document.createElement("li")
+        // CREATE DELETION INFORMATION
         newDel.innerHTML = `<div class="ms-2 me-auto">
         ${list} is removed from the user list.</div>`
         newDel.setAttribute("class", "list-group-item d-flex justify-content-between align-items-center")
         let history = document.getElementById("history-list")
-        console.log(newDel)
+        // RENDER THE DELETION OPERATION ON HISTORY AREA
         history.appendChild(newDel)
-    } else {
-        list.forEach(function (item) {
-            const newHi = document.createElement("li")
-            newHi.innerHTML = `<div class="ms-2 me-auto">
-        ${item.name} is registered as a new user. Current Balance: ${item.balance}</div>`
-            newHi.setAttribute("class", "list-group-item d-flex justify-content-between align-items-center")
-            subscriber.appendChild(newHi)
-        })
+        // TRANSACTION
+    } else if (list === "addTransaction") {
+        let fromWho = document.getElementById("fromWho")
+        let toWho = document.getElementById("toWho")
+        let value = document.getElementById("value").value
+        let fromWhoName = fromWho.options[fromWho.selectedIndex].value
+        let toWhoName = toWho.options[toWho.selectedIndex].value
+        const historyList = document.getElementById("history-list")
+        // CREATE NEW ELEMENT FOR TRANSACTION
+        const newLi = document.createElement("li")
+        // CREATE TRANSACTION INFORMATION
+        newLi.innerHTML = `<div class="ms-2 me-auto">
+        ${fromWhoName} sent to ${toWhoName} : ${value}</div><button class="btn btn-danger btn-sm" id="cancelButton" onclick="cancelTransaction(this.id)">Cancel</button>`
+        newLi.setAttribute("class", "list-group-item d-flex justify-content-between align-items-center")
+        // RENDER THE TRANSACTION OPERATION ON HISTORY AREA
+        historyList.appendChild(newLi)
+    }
+    // OTHER OPERATIONS
+    else {
+        // CREATE NEW ELEMENT
+        const newHi = document.createElement("li")
+        // CREATE NEW ELEMENT
+        newHi.innerHTML = `<div class="ms-2 me-auto">
+        ${list} is registered as a new user. Current Balance: ${addition}</div>`
+        newHi.setAttribute("class", "list-group-item d-flex justify-content-between align-items-center")
+        // RENDER THE IT ON HISTORY AREA
+        subscriber.appendChild(newHi)
     }
 }
 
 function removeUser(id) {
+    // DELETING USER
     const name = state.userList.filter((item) => item.id === id)[0].name
+    // RENDER IT ON HISTORY AREA
     Hi(name, "del")
-    state.userList.forEach(function (item) {
-        state.userList.filter((item) => item.id === id)
-        state.userList = state.userList.filter((item) => item.id !== id)
-        updateUserList()
-        updateSelectList()
-    })
+    // UPDATE USERLIST
+    state.userList = state.userList.filter((item) => item.id !== id)
+    updateUserList()
+    updateSelectList()
 }
 
-function renderUserList() {
-    const subscribers = [document.getElementById("user-list"), document.getElementById("history-list")];
-    subscribers.forEach(function (subscriber) {
-        subscriber.innerHTML = ""
-        if (subscriber.getAttribute("id") === "user-list") {
-            Li(state.userList, subscriber)
-        } else if (subscriber.getAttribute("id") === "history-list") {
-            Hi(state.userList, subscriber)
-        }
-    })
+function renderUserList(action, action2) {
+    const userList = document.getElementById("user-list")
+    const historyList = document.getElementById("history-list")
+    // RENDER ON USERLIST AND HISTORY AREA
+    Li(state.userList, userList)
+    if (action === addTransaction) {
+        Hi(addTransaction)
+    } else {
+        Hi(action, historyList, action2)
+    }
 }
 
 function updateUserList() {
     const subscriber = document.getElementById("user-list")
     subscriber.innerHTML = ""
     if (subscriber.getAttribute("id") === "user-list") {
+        // UPDATE USERLIST
         Li(state.userList, subscriber)
     }
 }
 
+// STATE SETTING
 function setState(stateName, newValue) {
-    state[stateName] = newValue;
-    renderUserList();
+    state[stateName] = newValue
 }
 
-let userId = 0
+let userId = 1000
 function addUser() {
     const userName = document.getElementById("userName").value
-    userId++
     const userBalance = document.getElementById("userBalance").value;
-    setState("userList", [
-        ...state.userList,
-        {
-            id: userId,
-            name: userName,
-            balance: userBalance
-        },
+    // CREATE NEW USER CONSIDERING TO THE FORM AND CREATE AN ID
+    if (userName && userBalance) {
+        userId++
+        setState("userList", [
+            ...state.userList,
+            {
+                id: userId,
+                name: userName,
+                balance: userBalance
+            },
+        ])
+        renderUserList(userName, userBalance)
+        updateSelectList()
+        // ERROR HANDLING
+    } else { alert("Please make sure to fill the 'Name' and 'Balance' sections properly.") }
+}
+
+let transId = 0
+function addTransaction() {
+    transId++
+    let fromWho = document.getElementById("fromWho")
+    let toWho = document.getElementById("toWho")
+    let value = document.getElementById("value").value
+    let fromWhoId = fromWho.options[fromWho.selectedIndex].id
+    let toWhoId = toWho.options[toWho.selectedIndex].id
+    // CREATE NEW TRANSACTION ARRAY AND CREATE AN ID
+    setState("transaction", [
+        ...state.transaction, { transId, fromWhoId, toWhoId, value }
     ])
-    renderUserList()
-    updateSelectList()
+    if (fromWhoId && toWhoId && value && fromWhoId != toWhoId) {
+        // UPDATE BALANCES OF TO THE PARTIES
+        state.userList.forEach(function (item) {
+            if (item.id == fromWhoId) {
+                item.balance = parseInt(item.balance) - parseInt(value)
+            }
+            if (item.id == toWhoId) {
+                item.balance = parseInt(item.balance) + parseInt(value)
+            }
+        })
+        updateUserList()
+        renderUserList("addTransaction")
+        let button = document.getElementById("cancelButton")
+        button.id = transId
+        //ERROR HANDLING
+    } else if (fromWhoId && toWhoId && value && fromWhoId == toWhoId) {
+        alert("Sender and Recipient cannot be the same.")
+    } else {
+        alert("Please make sure to fill the transaction form properly.")
+    }
+}
+
+function cancelTransaction(id) {
+    const canceledTransaction = state.transaction.filter((item) => item.transId == id)[0]
+    let fromWhoId = canceledTransaction.fromWhoId
+    let toWhoId = canceledTransaction.toWhoId
+    let value = canceledTransaction.value
+    // CHECK IF USERS EXIST
+    state.userList.forEach(function (item) {
+        let found = true
+        if (item.id != fromWhoId) {
+            found = false
+        }
+        if (item.id != toWhoId) {
+            found = false
+        }
+        // UPDATE BALANCES OF TO THE PARTIES
+        if (found == true) {
+            if (item.id == fromWhoId) {
+                item.balance = parseInt(item.balance) + parseInt(value)
+            }
+            if (item.id == toWhoId) {
+                item.balance = parseInt(item.balance) - parseInt(value)
+                let cancelledButton = document.getElementById(id)
+                cancelledButton.disabled = true
+                cancelledButton.innerText = "Canceled"
+            }
+            //ERROR HANDLING
+        } else { alert("Cannot be canceled the transaction! Sender or recipient account does not exist!") }
+    })
+    updateUserList()
 }
 
 function updateSelectList() {
     let fromWho = document.getElementById("fromWho")
     let toWho = document.getElementById("toWho")
-    toWho.innerHTML = `<option selected>Please Select Recipient</option>`
-    fromWho.innerHTML = `<option selected>Please Select Sender</option>`
-    state.userList.forEach(function (item) {
-        let newSelectName1 = document.createElement("option")
-        newSelectName1.setAttribute("name", item.name)
-        newSelectName1.innerText = item.name
-        newSelectName1.setAttribute("id", item.id)
-        newSelectName1.setAttribute("balance", item.balance)
-        let newSelectName2 = document.createElement("option")
-        newSelectName2.setAttribute("name", item.name)
-        newSelectName2.innerText = item.name
-        newSelectName2.setAttribute("id", item.id)
-        newSelectName2.setAttribute("balance", item.balance)
-        toWho.appendChild(newSelectName1)
-        fromWho.appendChild(newSelectName2)
+    // FIND USERS FROM USERLIST
+    const elements = state.userList.map(function (item) {
+        const { name, id, balance } = item
+        return selectName = `<option name="${name} balance="${balance}" id="${id}">${name} </option>`
     })
+    // RENDER USERS TO SELECTLIST
+    fromWho.innerHTML = `<option disabled selected>Please Select Sender</option>` + elements.join("")
+    toWho.innerHTML = `<option disabled selected>Please Select Recipient</option>` + elements.join("")
 }
 
-
-function transaction() {
-
-
-
-
-    let fromWho = document.getElementById("fromWho")
-    let toWho = document.getElementById("toWho")
-    let value = document.getElementById("value")
-    let fromWhoName = fromWho.options[fromWho.selectedIndex].value
-    let toWhoName = toWho.options[toWho.selectedIndex].value
-    let fromWhoId = fromWho.options[fromWho.selectedIndex].id
-    let toWhoId = toWho.options[toWho.selectedIndex].id
-    let transactionValue = value.value
-
-    if (fromWhoName != "Please Select Sender" && toWhoName != "Please Select Recipient" && fromWhoName != toWhoName) {
-
-        state.userList.forEach(function (item) {
-            if (item.id == fromWhoId) {
-                item.balance = parseInt(item.balance) - parseInt(transactionValue)
-            }
-            if (item.id == toWhoId) {
-                item.balance = parseInt(item.balance) + parseInt(transactionValue)
-            }
-        })
-
-        if (fromWhoId && toWhoId && transactionValue) {
-            const historyList = document.getElementById("history-list")
-            const newLi = document.createElement("li")
-            newLi.innerHTML = `<div class="ms-2 me-auto">
-        ${fromWhoName} sent to ${toWhoName} : ${transactionValue}</div><button class="btn btn-danger btn-sm">Cancel</button>`
-            newLi.setAttribute("class", "list-group-item d-flex justify-content-between align-items-center")
-            historyList.appendChild(newLi)
-        }
-        updateUserList()
-    }
-}
 
 
 
